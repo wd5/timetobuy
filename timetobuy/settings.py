f15bc80cf8e97fce31abd519cb4e1a2d2b98c97f
cheguevara <mail@kvove.ru>
2013-01-08 19:05:37 +0400

    часы

--- timetobuy/settings.py
+++ timetobuy/settings.py
@@ -12,10 +12,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'timetobuy',                      # Or path to database file if using sqlite3.
-        'USER': 'timetobuy',                      # Not used with sqlite3.
-        'PASSWORD': 'timetobuy',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -161,3 +161,8 @@ LOGGING = {
         },
     }
 }
+
+try:
+    from yorcc.local_settings import *
+except ImportError:
+    pass
